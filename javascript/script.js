0668f8d4de455109b3ff78198eb67900b938f93d
GessioneSena <fgssena@gmail.com>
2022-06-13 20:51:32 -0300

    criação da pagina contato

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,21 @@
-/*Script pra rolar a página para cima*/
+/*Script do botão-levar para página de contato*/
 
-var btn = document.querySelector('#como-funciona');
-btn.addEventListener('click', rolarPagina);
+var btn_reserva = document.querySelector('#botao-reserva');
+btn_reserva.addEventListener('click', fzrReserva);
+
+function fzrReserva(){
+    window.open('contato.html', target='_self')
+}
+
+
+
+/*Script pra rolar a página para cima na página inicial*/
+
+var rolarPg = document.querySelector('#como-funciona');
+rolarPg.addEventListener('click', rolarPagina);
 
 
 function rolarPagina(){
     window.scrollTo(0, 650);
 }
+
